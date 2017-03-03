b115e6439900fef5e873d1ddaf613737b6fdcf91
Jesse Thomas <jayayetea@gmail.com>
2017-03-02 19:50:21 -0600

    stuff

--- document.js
+++ document.js
@@ -1,4 +1,4 @@
-var config = {
+/*var config = {
     apiKey: "AIzaSyAfrk7HHCm1qZYlyHgzif0umHkwvcC-oho",
     authDomain: "groupproject-d2bdc.firebaseapp.com",
     databaseURL: "https://groupproject-d2bdc.firebaseio.com",
@@ -16,19 +16,38 @@ database.ref().on("value", function(snapshot) {
 
 }, function(errorObject) {
   console.log("The read failed: " + errorObject.code);
-});
+});*/
 
 $("#submit-btn").on("click", function(event) {
 
 	event.preventDefault();
 
 	var employeeName = $("#employee-name").val().trim();
+	console.log(employeeName);
 	var employeeRole = $("#employee-role").val().trim();
+	console.log(employeeRole);
 	var startDate = $("#start-date").val().trim();
-	var newDate = moment().subtract(10, 'days').calendar();
-	console.log(newDate);
+	console.log(startDate);
+	//var newDate = moment().subtract(10, 'days').calendar();
+	//console.log(newDate);
 	var monthsWorked;
 	var monthlyRate = $("#monthly-rate").val().trim();
+	console.log(monthlyRate);
 	var totalBilled = monthlyRate * monthsWorked;
+
+	var newRow = $("<tr>");
+	var newName = $("<td>");
+	var newRole = $("<td>");
+	var newDate = $("<td>");
+	var newRate = $("<td>");
+
+	newName.text(employeeName);
+	newRole.text(employeeRole);
+	newDate.text(startDate);
+	newDate.text(monthlyRate);
+
+	newRow.append(newName, newRole, newDate, newRate);
+
+	$("#employee-table").append(newRow);
 
 });
