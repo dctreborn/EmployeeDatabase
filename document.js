7f5762a6e92ba9deb4101e9251829fa2c673d859
Jesse Thomas <jayayetea@gmail.com>
2017-03-02 21:17:27 -0600

    final

--- document.js
+++ document.js
@@ -12,33 +12,44 @@ var database = firebase.database();
 
 database.ref().on("child_added", function(snapshot) {
 
-	if ( ( snapshot.child("name").exists() ) && (snapshot.child("role").exists()) && (snapshot.child("startDate").exists()) && (snapshot.child("monthlyRate").exists())){
+	if ( ( snapshot.child("name").exists() ) && (snapshot.child("role").exists()) && (snapshot.child("startDate").exists()) && (snapshot.child("months").exists()) && (snapshot.child("monthlyRate").exists()) && (snapshot.child("totalBilled").exists())){
 
 		var employeeName = snapshot.val().name;
-		console.log(employeeName);
+		
 		var employeeRole = snapshot.val().role;
-		console.log(employeeRole);
+		
 		var startDate = snapshot.val().startDate;
-		console.log(startDate);
-		var newDate = moment().subtract(10, 'days').calendar();
-		console.log(newDate);
-		var monthsWorked;
+		
+		var monthsWorked = snapshot.val().months;
+
 		var monthlyRate = snapshot.val().monthlyRate;
-		console.log(monthlyRate);
-		var totalBilled = monthlyRate * monthsWorked;
+		
+		var totalBilled = snapshot.val().totalBilled;
+
+		var now = moment(new Date()); //todays date
+		
+		var end = moment(startDate); // start date
+		var duration = moment.duration(now.diff(end));
+		var months = duration.asMonths();
+		//console.log(months)
+
 
 		var newRow = $("<tr>");
 		var newName = $("<td>");
 		var newRole = $("<td>");
 		var newDate = $("<td>");
+		var newMonths = $("<td>");
 		var newRate = $("<td>");
+		var newTotal = $("<td>");
 
 		newName.text(employeeName);
 		newRole.text(employeeRole);
 		newDate.text(startDate);
+		newMonths.text(monthsWorked);
 		newRate.text(monthlyRate);
+		newTotal.text(totalBilled);
 
-		newRow.append(newName, newRole, newDate, newRate);
+		newRow.append(newName, newRole, newDate, newMonths, newRate, newTotal);
 
 		$("#employee-table").append(newRow);
 	}
@@ -52,30 +63,43 @@ $("#submit-btn").on("click", function(event) {
 	event.preventDefault();
 
 	var employeeName = $("#employee-name").val().trim();
-	console.log(employeeName);
+	//console.log(employeeName);
 	var employeeRole = $("#employee-role").val().trim();
-	console.log(employeeRole);
+	//console.log(employeeRole);
 	var startDate = $("#start-date").val().trim();
-	console.log(startDate);
+	//console.log(startDate);
 	var newDate = moment().subtract(10, 'days').calendar();
-	console.log(newDate);
+	//console.log(newDate);
 	var monthsWorked;
 	var monthlyRate = $("#monthly-rate").val().trim();
-	console.log(monthlyRate);
+	//console.log(monthlyRate);
+	
+
+	var now = moment(new Date()); //todays date
+	//console.log(now);
+	var end = moment(startDate); // another date
+	var duration = moment.duration(now.diff(end));
+	monthsWorked = duration.asMonths();
+	//console.log(months)
+
 	var totalBilled = monthlyRate * monthsWorked;
 
 	var newRow = $("<tr>");
 	var newName = $("<td>");
 	var newRole = $("<td>");
 	var newDate = $("<td>");
+	var newMonths = $("<td>");
 	var newRate = $("<td>");
+	var newTotal = $("<td>");
 
 	newName.text(employeeName);
 	newRole.text(employeeRole);
 	newDate.text(startDate);
+	newMonths.text(monthsWorked);
 	newRate.text(monthlyRate);
+	newTotal.text(totalBilled);
 
-	newRow.append(newName, newRole, newDate, newRate);
+	newRow.append(newName, newRole, newDate, newMonths, newRate, newTotal);
 
 	$("#employee-table").append(newRow);
 
@@ -84,7 +108,9 @@ $("#submit-btn").on("click", function(event) {
         name: employeeName,
         role: employeeRole,
         startDate: startDate,
-        monthlyRate: monthlyRate
+        months: monthsWorked,
+        monthlyRate: monthlyRate,
+        totalBilled: totalBilled
     });
 
 });
